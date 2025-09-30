adf0aca528c31a184ac841a45dae49d8c1d7e31e
wootaepark <pakdal99@gmail.com>
2025-09-30 14:29:23 +0900

    feat: sned face mesh landmarks from client via ws

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const WebSocket = require("ws");
+
 const app = express();
-
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+const server = http.createServer(app); // WebSocket 을 위한 HTTP 서버 객체
+const wss = new WebSocket.Server({server});
+
+// 얼굴 landmark 데이터 받기
+wss.on("connection", (socket) => {
+  socket.on("message", (data) =>{
+    console.log("Recieved landmarks: ", JSON.parse(data));
+  })
+});
 
 const PORT = 8000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
